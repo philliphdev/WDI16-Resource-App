96241bfec93803074be0c56954c8afacb36bd7d9
philliphdev <phannafo@us.ibm.com>
2018-06-25 19:00:24 -0400

    created and tested React Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Guests from './components/Guests'
+import Home from './components/Home'
+import Resources from './components/Resources'
+import Users from './components/Users'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/users" component={Users} />
+          <Route exact path="/guests" component={Guests} />
+          <Route path="/resources" component={Resources} />
+        </Switch>
+      </Router>
     );
   }
 }
